c090fc975aa81f19919f74b66f86131c076d39eb
JPiolho <783467+jpiolho@users.noreply.github.com>
2022-02-05 18:41:54 +0100

    Changed Game.Mod to only return the mod folder name. Added Game.ModFolder to return the full path. Added api doc for both of them.

--- QuakePlugins/API/Game.cs
+++ QuakePlugins/API/Game.cs
@@ -1,6 +1,7 @@
 ﻿using QuakePlugins.Engine;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,13 @@ namespace QuakePlugins.API
     /// <apiglobal />
     public static class Game
     {
-        public static string Mod => QEngine.GameGetGameDir();
+        /// <summary>
+        /// Returns the current game. Eg: id1, mg1, rogue, fortress...
+        /// </summary>
+        public static string Mod => Path.GetFileName(QEngine.GameGetGameDir());
+        /// <summary>
+        /// Returns the full path to the mod folder.
+        /// </summary>
+        public static string ModFolder => QEngine.GameGetGameDir();
     }
 }
