911c5729f627b09ba9af6accbba3bb815b55c419
JPiolho <783467+jpiolho@users.noreply.github.com>
2022-02-07 21:05:41 +0100

    Fixed Utils.MarshalStringToHGlobalUTF8 not adding a zero terminator at the end

--- QuakePlugins/Utils.cs
+++ QuakePlugins/Utils.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using QuakePlugins.API;
+using QuakePlugins.Engine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +15,14 @@ namespace QuakePlugins
         public static IntPtr MarshalStringToHGlobalUTF8(string text)
         {
             var bytes = Encoding.UTF8.GetBytes(text);
-            var ptr = Marshal.AllocHGlobal(bytes.Length);
+            var ptr = Marshal.AllocHGlobal(bytes.Length+1);
             Marshal.Copy(bytes, 0, ptr, bytes.Length);
+            
+            // Set zero terminator
+            unsafe
+            {
+                *(byte*)(ptr.ToInt64() + bytes.Length) = 0;
+            }
 
             return ptr;
         }
